61541ef7c4b89e961d32f25e7071978ee404de1d
29rifjanj <rjndla@gmail.com>
2023-09-07 02:24:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,383 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 import './App.css';
+import bg from './assets/img/bg-1.svg';
+import profile from './assets/img/profile.png';
+import Navbar from './Navbar';
+import 'remixicon/fonts/remixicon.css'
+import { DarkModeProvider, useDarkMode } from './DarkModeContext';
 
 function App() {
+  const { isDarkMode } = useDarkMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+     <div className={isDarkMode ? 'dark' : ''}>
+      <Navbar />
+      <section
+        className="
+                    h-screen
+                    dark:bg-slate-900
+                  "
+      >
+        <div className='
+                mx-auto
+                px-5
+                pt-5
+                md:py-56
+                container'>
+          <div
+            className="
+                    grid 
+                    grid-cols-1
+                    md:grid-cols-2
+                    grid-rows-1 
+                    gap-4"
+          >
+            <div>
+              <hr className="mb-2 w-36 h-0.5 bg-black"></hr>
+              <h4
+                className="
+                          text-2xl
+                          font-medium
+                          dark:text-gray-500 
+                          "
+              >
+                Hi 👋!
+              </h4>
+              <h1
+                  className="
+                      text-4xl 
+                      font-bold 
+                      tracking-tight 
+                      text-gray-900 
+                      sm:text-6xl 
+                      sm:text-center 
+                      md:text-left
+                      md:mx-auto
+                      dark:text-gray-500 "
+              >
+                  I'am <span className="text-emerald-500">Rifjan </span>,
+              </h1>
+              <h1 className=' 
+                  text-4xl 
+                  font-bold 
+                  tracking-tight 
+                  text-gray-900 
+                  sm:text-6xl 
+                  sm:text-center 
+                  md:text-left
+                  dark:text-gray-500 '>
+                  Web Developer
+              </h1>
+                
+              <p
+                className="
+                          font-xl
+                          font-medium
+                          leading-relaxed
+                          text-gray-500 
+                          my-5
+                          md:text-justify
+                          w-full
+                          md:w-1/2
+                          "
+              >
+                An web developer with 1 years of experience in
+                <strong>web application development</strong>.And also
+                interest to learn more about new technology and
+                <strong> UI/UX Design</strong>
+              </p>
+              <a
+                href="#"
+                className="rounded-md bg-emerald-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-400"
+              >
+                Get started
+              </a>
+              <div
+                className="
+                        flex 
+                        items-center 
+                        gap-9 
+                        mt-14"
+              >
+                <span
+                  className="
+                          text-sm
+                          font-bold 
+                          text-gray-500"
+                >
+                  Follow me :{" "}
+                </span>
+                <span
+                  className="
+                            text-xl 
+                            text-gray-500"
+                >
+                  <a href="#">
+                    <i className="ri-linkedin-box-line"></i>
+                  </a>
+                </span>
+                <span className="text-xl text-gray-500">
+                  <a href="#">
+                    <i className="ri-instagram-line"></i>
+                  </a>
+                </span>
+                <span className="text-xl text-gray-500">
+                  <a href="#">
+                    <i className="ri-github-line"></i>
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div
+                className="
+                          relative 
+                          h-[400px] 
+                          bg-center 
+                          bg-no-repeat 
+                          bg-cover 
+                          rounded-3xl"
+                style={{ backgroundImage: `url(${bg})` }}
+              >
+                <img
+                  src={profile}
+                  className="
+                        absolute 
+                        top-[150px] 
+                        left-1/2 
+                        transform 
+                        -translate-x-1/2 
+                        -translate-y-1/2 
+                        h-[500px] 
+                        w-50"
+                  alt="Profile Image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* START EXPERIENCE */}
+      <section>
+        <div
+          className="
+          container 
+          mx-auto
+          px-5
+          py-5
+          md:py-12
+      "
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="">
+            <h1
+              className="
+              text-4xl
+              font-bold
+              pb-2
+              text-center
+            "
+            >
+              EXPERIENCE
+            </h1>
+            <hr
+              className="
+              mb-2 
+              w-24
+              h-1
+              bg-emerald-400
+              mx-auto
+          "
+            />
+          </div>
+          <div className="relative my-6">
+            {/* Vertical Line in the Middle */}
+            <div
+              className="absolute inset-0 flex items-center justify-center"
+              aria-hidden="true"
+            >
+              <div className="h-full w-px bg-gray-300 relative hidden md:block">
+                {/* Dot for the first content's header */}
+                <div className="absolute -left-1.5 w-2.5 h-2.5 bg-black rounded-full top-0"></div>
+
+                {/* Dot for the second content's header */}
+                <div className="absolute -left-1.5 w-2.5 h-2.5 bg-black rounded-full top-[70%]"></div>
+              </div>
+            </div>
+
+            {/* Content on Left and Right */}
+            <div className="relative flex flex-col space-y-16">
+              {/* First Content on the Left */}
+              <div className="flex items-center">
+                <div className="w-full md:w-1/2 pr-4 ">
+                  {/* Job Title & Duration */}
+                  <h4 className="font-bold text-justify">
+                    WebApps Engineer | PT Global Data Inspirasi
+                  </h4>
+                  <p className="text-sm text-gray-500 text-justify">
+                    Okt 2022 - Jan 2023 · 4 bln
+                  </p>
+
+                  {/* Job Description */}
+                  <ol className="list-decimal pl-5 space-y-2 text-justify mt-3">
+                    <li className="text-sm text-gray-500 leading-relaxed">
+                      Membantu pengerjaan development di bidang software
+                      development untuk mendukung pengembangan produk dan
+                      project Datains.
+                    </li>
+                    <li className="text-sm text-gray-500 leading-relaxed">
+                      Membantu pengerjaan development di bidang software
+                      development untuk mendukung pengembangan produk dan
+                      project Datains.
+                    </li>
+                    {/* More descriptions if needed */}
+                  </ol>
+                </div>
+              </div>
+              {/* Second Content on the Right */}
+              <div className="flex items-center justify-end">
+                <div className="w-full md:w-1/2 md:pl-4">
+                  {/* Job Title & Duration */}
+                  <h4 className="font-bold">Web Developer | Greensoft</h4>
+                  <p className="text-sm text-gray-500">
+                    Feb 2022 - Mei 2022 · 4 bln
+                  </p>
+
+                  {/* Job Description */}
+                  <ol className="list-decimal px-3 space-y-24 text-left mt-3">
+                    <li className="text-sm text-gray-500 leading-relaxed">
+                      Membantu pengerjaan development di bidang software
+                      development untuk mendukung pengembangan produk dan
+                      project.
+                    </li>
+                    {/* More descriptions if needed */}
+                  </ol>
+                </div>
+              </div>
+              {/* Third Content on the Left */}
+              <div className="flex items-center">
+                <div className="w-full md:w-1/2 pr-4">
+                  {/* Job Title & Duration */}
+                  <h4 className="font-bold text-justify">
+                    Web Developer | PT Global Intermedia
+                  </h4>
+                  <p className="text-sm text-gray-500 text-justify">
+                    Okt 2022 - Jan 2023 · 4 bln
+                  </p>
+
+                  {/* Job Description */}
+                  <ol className="list-decimal pl-5 space-y-2 text-justify mt-3">
+                    <li className="text-sm text-gray-500 leading-relaxed">
+                      Membantu pengerjaan development di bidang software
+                      development untuk mendukung pengembangan produk dan
+                      project Datains.
+                    </li>
+                    <li className="text-sm text-gray-500 leading-relaxed">
+                      Membantu pengerjaan development di bidang software
+                      development untuk mendukung pengembangan produk dan
+                      project Datains.
+                    </li>
+                    {/* More descriptions if needed */}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* END EXPERIENCE */}
+      {/* START PROJECTS */}
+      <section className='bg-slate-100 pt-16 pb-32'>
+        <div
+          className=" 
+          container 
+          mx-auto
+         "
+        >
+          <div className="w-full px-4">
+            <h1
+              className="
+                  text-4xl
+                  font-bold
+                  pb-2
+                  text-center
+                "
+            >
+              PROJECTS
+            </h1>
+            <hr
+              className="
+                  mb-2 
+                  w-24
+                  h-1
+                  bg-emerald-400
+                  mx-auto
+              "
+            />
+          </div>
+          <div className='w-full flex flex-wrap justify-center px-4'>
+            <div className='mb-12 p-4 md:w-1/2'>
+              <div className='rounded-md shadow-md overflow-hidden'>
+                <img className='w-full'></img>
+              </div>
+                <h3 className='font-semibold text-xl text-dark mt-5 mb-3'>Landing Page</h3>
+                <p className='font-medium text-base text-slate-400 text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+                </p>
+            </div>
+            <div className='mb-12 p-4 md:w-1/2'>
+              <div className='rounded-md shadow-md overflow-hidden'>
+                <img className='w-full'></img>
+              </div>
+                <h3 className='font-semibold text-xl text-dark mt-5 mb-3'>Landing Page</h3>
+                <p className='font-medium text-base text-slate-400 text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+                </p>
+            </div>
+          </div>
+            
+        </div>
+      </section>
+      {/* FINISH PROJECTS */}
+      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+          <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+              <div className="sm:flex sm:items-center sm:justify-between">
+                  <a href="#" className="flex items-center mb-4 sm:mb-0">
+                      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Rifjan Jundila</span>
+                  </a>
+                  <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                      <li>
+                          <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                      </li>
+                      <li>
+                          <a href="#" className="mr-4 hover:underline md:mr-6">Experience</a>
+                      </li>
+                      <li>
+                          <a href="#" className="mr-4 hover:underline md:mr-6 ">Project</a>
+                      </li>
+                  </ul>
+              </div>
+              <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+              <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">@rifjanj29™</a>. All Rights Reserved.</span>
+          </div>
+      </footer>
+     </div>
+
+
+    </>
   );
 }
 
-export default App;
+
+function RootApp() {
+  return (
+      <DarkModeProvider>
+          <App />
+      </DarkModeProvider>
+  );
+}
+
+export default RootApp;
---
+++ src/Navbar.js
@@ -0,0 +1,96 @@
+import { Fragment } from 'react'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useDarkMode } from './DarkModeContext';
+
+const navigation = [
+  { name: 'ABOUT', href: '#', current: true },
+  { name: 'EXPERIENCE', href: '#', current: false },
+  { name: 'PROJECTS', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  return (
+    <Disclosure as="nav" className="bg-white-800 border-b-2 dark:bg-slate-900">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-between sm:items-stretch sm:justify-center">
+              
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'border-b-2 border-emerald-500 text-gray-700 dark:text-white' : 'text-gray-900 hover:border-b-2 hover:border-emerald-500 hover:text-gray-700 dark:text-white',
+                          ' px-3 py-2 text-sm font-medium dark:text-white'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              
+              </div>
+              <div className="flex align-middle justify-center">
+                  <button
+                      onClick={toggleDarkMode}
+                      className="hs-dropdown-toggle hs-dark-mode group flex items-center text-gray-600 hover:text-blue-600 font-medium dark:text-gray-400 dark:hover:text-gray-500">
+                    <svg className="dark:hidden block w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
+                    </svg>
+                    <svg className="dark:block hidden w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+                    </svg>
+                    <p className='dark:text-white text-gray-700 text-sm mx-2'> {isDarkMode ? "Light Mode" : "Dark Mode"}</p>
+                  </button>
+
+              </div>            
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
